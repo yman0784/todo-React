079fb606008a3c33b6af5f5fa70f1ca823497947
yman0784 <117918439+yman0784@users.noreply.github.com>
2023-09-26 12:21:58 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div></div>
+      <div></div>
+    </>
+  );
+};
